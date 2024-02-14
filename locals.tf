856a4d4fbdf578ac910fc49304df12a0748a4e34
LukePetzer-ixo <luke.petzer@ixo.world>
2024-02-14 12:57:03 +0200

    jetstack

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  ixo_helm_chart_repository          = "https://github.com/ixofoundation/ixo-helm-charts"
-  cert_manager_helm_chart_repository = "https://charts.jetstack.io"
+  ixo_helm_chart_repository      = "https://github.com/ixofoundation/ixo-helm-charts"
+  jetstack_helm_chart_repository = "https://charts.jetstack.io"
 }
