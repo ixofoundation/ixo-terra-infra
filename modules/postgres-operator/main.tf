2097809fcba11d37ad96aaba0a52273e20fea856
LukePetzer-ixo <luke.petzer@ixo.world>
2024-02-16 14:01:26 +0200

    Cleanup

--- modules/postgres-operator/main.tf
+++ modules/postgres-operator/main.tf
@@ -7,30 +7,32 @@ terraform {
 }
 
 resource "kubernetes_config_map_v1" "init_sql" {
+  for_each = { for cluster in var.clusters : cluster.pg_cluster_namespace => cluster }
   metadata {
-    name      = "${var.cluster.pg_cluster_name}-init-sql"
-    namespace = var.cluster.pg_cluster_namespace
+    name      = "${each.value.pg_cluster_name}-init-sql"
+    namespace = each.value.pg_cluster_namespace
   }
 
   data = {
-    "init.sql" = var.cluster.initSql != null ? var.cluster.initSql : ""
+    "init.sql" = each.value.initSql != null ? each.value.initSql : ""
   }
 }
 
 resource "kubectl_manifest" "cluster" {
+  for_each   = { for cluster in var.clusters : cluster.pg_cluster_namespace => cluster }
   depends_on = [kubernetes_config_map_v1.init_sql]
   yaml_body = templatefile("${path.module}/crds/cluster.yml",
     {
-      pg_cluster_name      = var.cluster.pg_cluster_name
-      pg_namespace         = var.cluster.pg_cluster_namespace
-      pg_image             = var.cluster.pg_image
-      pg_image_tag         = var.cluster.pg_image_tag
-      pg_version           = var.cluster.pg_version
-      pg_instances         = var.cluster.pg_instances
-      pg_users             = var.cluster.pg_users
-      pgbackrest_image     = var.cluster.pgbackrest_image
-      pgbackrest_image_tag = var.cluster.pgbackrest_image_tag
-      pgbackrest_repos     = var.cluster.pgbackrest_repos
+      pg_cluster_name      = each.value.pg_cluster_name
+      pg_namespace         = each.value.pg_cluster_namespace
+      pg_image             = each.value.pg_image
+      pg_image_tag         = each.value.pg_image_tag
+      pg_version           = each.value.pg_version
+      pg_instances         = each.value.pg_instances
+      pg_users             = each.value.pg_users
+      pgbackrest_image     = each.value.pgbackrest_image
+      pgbackrest_image_tag = each.value.pgbackrest_image_tag
+      pgbackrest_repos     = each.value.pgbackrest_repos
     }
   )
 }
