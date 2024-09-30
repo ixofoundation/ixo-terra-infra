e45677a830c92b55cb9a16b7e9ecdbc1d7d3482c
LukePetzer-ixo <luke.petzer@ixo.world>
2024-09-30 13:04:45 +0200

    Add SLACK_WEBHOOK_URL to ixo_kyc_server, added descriptors

--- modules/argocd_application/variables.tf
+++ modules/argocd_application/variables.tf
@@ -17,12 +17,15 @@ variable "application" {
 }
 
 variable "kv_defaults" {
-  default = {}
+  default     = {}
+  description = "Optional: This is the default vault keys on initial creation of the Secret."
 }
 
+#
 variable "create_kv" {
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
+  description = "Optional: If true, will create a Secret on Vault for this app, uses kv_defaults for default values on creation."
 }
 
 variable "argo_namespace" {
